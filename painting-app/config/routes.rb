03a982cce2a141175d5cf43f9992c17064a530cf
dangnhon <dang.nhon12345@yahoo.com>
2021-02-12 18:27:59 -0800

    more back end auth stuff

--- painting-app/config/routes.rb
+++ painting-app/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :paintings
   resources :topics
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #resources :users
+  resources :users, only: [:create]
+  post '/login', to: 'auth#create'
+  get '/profile', to: 'users#profile'
+  
 end
