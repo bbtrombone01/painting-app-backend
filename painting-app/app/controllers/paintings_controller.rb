4ba22db0ef5f47e48b80a238f0b004486f312a21
racross1 <racross1@gmail.com>
2021-02-17 15:18:23 -0800

    view paintings

--- painting-app/app/controllers/paintings_controller.rb
+++ painting-app/app/controllers/paintings_controller.rb
@@ -1,19 +1,23 @@
 class PaintingsController < ApplicationController
-  skip_before_action :authorized, only: [:create]
+  skip_before_action :authorized, only: [:create, :index]
 
   def index 
     paintings = Painting.all 
-    render json: paintings
+    all_paintings = []
+    paintings.each do |p|
+      result = {image: rails_blob_path(p.image, only_path: true), user_id: p.user_id,  topic_id: p.topic_id}
+      all_paintings << result 
+    end 
+    render json: all_paintings
   end 
   
-    def create
-        painting = Painting.create(painting_params)
+  def create
+    painting = Painting.create(painting_params)
         
-
-        result = {image: rails_blob_path(painting.image, only_path: true), user_id: painting.user_id,  topic_id: painting.topic_id}
+    result = {image: rails_blob_path(painting.image, only_path: true), user_id: painting.user_id,  topic_id: painting.topic_id}
         
-        render json: result
-    end
+    render json: result
+  end
     
       private
         def painting_params
