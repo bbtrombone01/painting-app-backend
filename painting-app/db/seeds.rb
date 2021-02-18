d2c3219b4dbd3768d804a7b8a36eb2856f1922d8
racross1 <racross1@gmail.com>
2021-02-18 15:16:14 -0800

    Topic seeds

--- painting-app/db/seeds.rb
+++ painting-app/db/seeds.rb
@@ -8,6 +8,12 @@ t1 = Topic.create(topic: "happy")
 t2 = Topic.create(topic: "sad")
 t3 = Topic.create(topic: "angry")
 t4 = Topic.create(topic: "excited")
+t5 = Topic.create(topic: "bored")
+t6 = Topic.create(topic: "stressed")
+t7 = Topic.create(topic: "procrastinate-y")
+t8 = Topic.create(topic: "grateful")
+t9 = Topic.create(topic: "curious")
+t10 = Topic.create(topic: "silly")
 
 # p1 = Painting.create( user_id: u1.id, topic_id: t1.id)
 
